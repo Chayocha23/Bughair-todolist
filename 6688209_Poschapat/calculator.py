f939ad1751cf590e0db7b264a5d0030849f09995
Poschapat <poschapat1@gmail.com>
2026-01-27 17:21:26 +0000

    Revert "Add velocity calculation to calculator"
    This reverts commit 6c3d85ee090a2fe6df89f158097c4f0270b2d778.

--- 6688209_Poschapat/calculator.py
+++ 6688209_Poschapat/calculator.py
@@ -58,13 +58,6 @@ def divide(a, b):
     return a / b
 
 
-def calculate_velocity(distance: float, time: float) -> float:
-    if time <= 0:
-        raise ValueError("Time must be greater than zero")
-    return distance / time
-
-
-
 def main():
     """Interactive calculator."""
     print("Simple Calculator")
